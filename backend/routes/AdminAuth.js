6b82d6638fb8917fc07ebf120940e1461d839508
frankosparks-prog <mainafrank400@gmail.com>
2025-09-24 23:10:56 +0300

    Installed bcryptjs

--- backend/routes/AdminAuth.js
+++ backend/routes/AdminAuth.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Admin = require("../models/Admin");
 const authenticateToken = require("../authToken"); // Assuming authenticateToken is your middleware to verify JWT
